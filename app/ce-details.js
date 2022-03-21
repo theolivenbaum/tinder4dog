504310c0f23de27c0ecefdeeb02f6c71744c03be
theolivenbaum <rafael@rafael.aero>
2022-03-21 11:06:09 +0100

    added missing folder

---
+++ app/ce-details.js
@@ -0,0 +1,35 @@
+customElements.define('tinder4dog-details', class extends HTMLElement {
+  constructor() {
+    super();
+    this._carousel = this.querySelector('tinder4dog-carousel');
+  }
+
+  connectedCallback() {
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  set data(value) {
+    this._data = value;
+    this._updateBindings();
+  }
+
+  _updateBindings() {
+    if(!this.data) return;
+    this._carousel.selected = 0;
+    while(this._carousel.firstChild) this._carousel.removeChild(this._carousel.firstChild);
+    for(let imgSrc of this.data.images) {
+      const div = document.createElement('div');
+      div.classList.add('carousel__item');
+      div.style.backgroundImage = `url('${imgSrc}')`;
+      this._carousel.appendChild(div);
+    }
+    this.querySelector('.item__details__name').textContent = this.data.name;
+    this.querySelector('.item__details__age').textContent = this.data.age;
+    this.querySelector('.item__details__job').textContent = (this.data.university ? "⚒️ " : "") + this.data.job + " " + (this.data.university ? "🎓 " + this.data.university : "");
+    this.querySelector('.item__details__distance').textContent = `${this.data.distance} kilometers away`;
+    this.querySelector('.description').textContent = this.data.description;
+  }
+});
---
+++ app/sw.js
@@ -0,0 +1,42 @@
+(function () {
+  const VERSION = '{%VERSION%}';
+  const files = [
+    'images/favicon.svg',
+    'images/header.jpg',
+    'images/paw-heart.png',
+    'images/paw-heart.svg',
+
+    'app.js',
+    'ce-carousel.js',
+    'ce-details.js',
+    'ce-item.js',
+    'custom-elements.min.js',
+    'helper.js',
+    './',
+    'manifest.json',
+    'styles.css'
+  ];
+
+  self.oninstall = event => event.waitUntil(
+    caches.open(`tinder4dog-${VERSION}`)
+      .then(cache => cache.addAll(files))
+  );
+
+  self.onactivate = event => event.waitUntil(
+    caches.keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames
+            .map(c => c.split('-'))
+            .filter(c => c[0] === 'tinder4dog')
+            .filter(c => c[1] !== VERSION)
+            .map(c => caches.delete(c.join('-')))
+        )
+      )
+  );
+
+  self.onfetch = event => event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+})();
